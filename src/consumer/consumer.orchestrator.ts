0fda567fea08044251c9a81f0f188a01f24b2a9c
hafemann <hafemann@sipgate.de>
2024-05-15 22:08:16 +0200

    :zap: move consumer connect to later point of nest lifecycle

--- src/consumer/consumer.orchestrator.ts
+++ src/consumer/consumer.orchestrator.ts
@@ -1,19 +1,37 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 import { Consumer, ConsumerRunConfig, Kafka } from 'kafkajs';
 import { KafkaConsumerInjectable } from './consumer';
 import { KafkaInjectableConfig } from './consumer.decorator';
 
 @Injectable()
-export class ConsumerOrchestrator {
+export class ConsumerOrchestrator
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
   private readonly consumers: Consumer[] = [];
   @Inject('kafka:instance') kafka: Kafka;
+
+  async onApplicationBootstrap() {
+    for (const consumer of this.consumers) {
+      await consumer.connect();
+    }
+  }
+
+  async onApplicationShutdown() {
+    for (const consumer of this.consumers) {
+      await consumer.disconnect();
+    }
+  }
 
   public async registerConsumer(
     config: KafkaInjectableConfig,
     provider: KafkaConsumerInjectable,
   ) {
     const consumer = this.kafka.consumer(config.consumerConfig);
-    await consumer.connect();
     await consumer.subscribe(config.subscribeConfig);
 
     const consumerRunConfig: ConsumerRunConfig = {};
